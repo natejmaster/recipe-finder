10360015f5debe30afb1461906882e7bf70c2cf6
natejmaster@gmail.com <natejmaster@gmail.com>
2023-08-01 14:09:33 -0500

    Added links to traverse between HTML pages, slightly adapted footer to stay at bottom of page regardless of rendering. Added message when there are no favorites. Added remove from favorites button to cards on favorites page

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -4,6 +4,7 @@ $(function() {
     let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
     // Clear the existing favorite recipe container
     let favoriteContainer = $("#favorite-container");
+    let noFavoritesMessage = $("#no-favorites-message");
     favoriteContainer.empty();
     // Loop through each saved favorite recipe and create a card for it
     for (let i = 0; i < favorites.length; i++) {
@@ -22,8 +23,21 @@ $(function() {
       let img = $("<img>")
         .addClass("py-2 w-80 h-80 m-auto flex justify-center")
         .attr("src", cardData.img);
-      card.append(title, img, url);
+      let removeBtn = $("<button>")
+      .addClass("text-xl text-white text-center bg-blue-500 hover:animate-pulse rounded lg:w-full")
+      .on("click", function() {
+        favorites.splice(i, 1);
+        localStorage.setItem("favorites", JSON.stringify(favorites));
+        displayFavorites();
+      })
+      .text("Remove this recipe from my favorites");
+      card.append(title, img, url, removeBtn);
       favoriteContainer.append(card);
+      if (favorites.length > 0) {
+        noFavoritesMessage.hide();
+      } else {
+        noFavoritesMessage.show();
+      }
     }
   }
   displayFavorites();
