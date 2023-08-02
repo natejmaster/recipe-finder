d725787e94fab6a3647073223ec4e5989c093838
Jack Ceretti <jackceretti@Jacks-MacBook-Pro.local>
2023-08-01 19:34:19 -0500

    commented out most of the favorites.js file

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -1,6 +1,7 @@
 $(function() {
 
   function displayFavorites() {
+    // grabs the favorites string and stores it in the favorites variable
     let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
     // Clear the existing favorite recipe container
     let favoriteContainer = $("#favorite-container");
@@ -11,10 +12,12 @@ $(function() {
       let card = $("<div>")
         .addClass("flex flex-col shadow-xl w-full card p-2 m-2 border-4 border-blue-500 border-solid rounded-lg lg:flex-row lg:flex-wrap")
         .attr("id", `fav-card-${i}`);
+        // creates html elements for cardData and stores them at cardData.title
       let title = $("<h3>")
         .addClass("text-blue-500 w-full text-xl text-center underline")
         .text(cardData.title.length > 32 ? cardData.title.substring(0, 32) + "..." : cardData.title);
       title.attr("data-title", cardData.title);
+      //creates html elements that when clicked on direct the user to cardData.url
       let url = $("<a>")
         .addClass("text-xl text-white text-center bg-blue-500 hover:animate-pulse rounded lg:w-full")
         .attr({ href: cardData.url, target: "_blank" })
